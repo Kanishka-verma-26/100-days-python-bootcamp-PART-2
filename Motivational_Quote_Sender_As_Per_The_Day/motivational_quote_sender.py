b02fc321c064c7d41a736e95665d3f0cc75c103a
Kanishka-verma-26 <kanishka@Hashstudioz.com>
2022-03-30 15:14:23 +0530

    Motivational Quote as per the day

---
+++ Motivational_Quote_Sender_As_Per_The_Day/motivational_quote_sender.py
@@ -0,0 +1,29 @@
+import random
+import smtplib
+import datetime
+
+my_email = "khashstudioz@gmail.com"
+password = "Hash@123"
+
+week_days = ['Monday',"Tuesday", "Wednesday","Thrusday", "Friday","Saturday", "Sunday"]
+
+now = datetime.datetime.now()
+# print(now.day)
+# print(now.month)
+# print(now.year)
+day = week_days[now.weekday()]
+
+with open("quotes.txt") as quote_file:
+    all_quotes = quote_file.readlines()
+    quote = random.choice(all_quotes)
+print(quote)
+
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()                       # securing our connection
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="kanishka@hashstudioz.com",
+        msg=f"Subject:{day} Motivation\n\n{quote}."
+    )
+    print("mail sent")
